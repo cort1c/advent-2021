9221842ba180bea773696f2702c6ed51def74b45
Justin Felker <justin.felker@gmail.com>
2021-12-02 01:04:35 -0600

    Adding day two solutions.

---
+++ day2/day2-1.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Command struct {
+	Direction string
+	Amount    int
+}
+
+func readInput() ([]Command, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var inputs []Command
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		parts := strings.Fields(input)
+		if len(parts) != 2 {
+			continue
+		}
+		amount, err := strconv.Atoi(parts[1])
+		if err != nil {
+			continue
+		}
+		inputs = append(inputs, Command{parts[0], amount})
+	}
+	return inputs, nil
+}
+
+func main() {
+	inputs, err := readInput()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	horizontal, vertical := 0, 0
+	for _, input := range inputs {
+		switch input.Direction {
+		case "forward":
+			horizontal += input.Amount
+		case "down":
+			vertical += input.Amount
+		case "up":
+			vertical -= input.Amount
+		}
+	}
+	fmt.Printf("result: horizontal = %d, vertical = %d, final = %d\n", horizontal, vertical, horizontal*vertical)
+}
